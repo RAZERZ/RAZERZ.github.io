98d2b486a2d7e1e4555f03c22a16a08cab60f1c8
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-01-05 22:32:50 +0100

    Added personally picked projects.

--- scripts/events.js
+++ scripts/events.js
@@ -9,6 +9,12 @@ function getHtmlCont(module) {
 
 function pgProjects() {
     contentContainer.innerHTML = getHtmlCont("projects");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],22, 1, "<br>Ticketing system for a local computer store. Eased their workflow and enabled backups.");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],18, 1, "<br>Installation script for pwnpi that simplified the installation process. The community found the installation process complicated.");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],17, 1, "<br>Pulls flight data form flightaware for use with phpVMS. This project was done for the flight simulation community, who found it very useful.");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],10, 1, "<br>A startup idea that was built as a concept for a school project.");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],2, 1, "<br>Device tree for the Samsung Galaxy S4-LTE-A. This was used for compiling custom operating systems.");
+    createProjList(document.getElementsByClassName("personalSelect")[0].children[1],7, 1, "<br>Kernel for the Samsung Galaxy S4-LTE-A. This was also used for the compilation of custom operating system, as well as tweaking clockspeed of the CPU.");
     for(var i = 0; getPopularProjects(projectsList).length > i; i++) {
         createProjList(document.getElementsByClassName("mostPop")[0].children[1], getPopularProjects(projectsList)[i]);
     }
--- scripts/global.js
+++ scripts/global.js
@@ -22,46 +22,77 @@ function getPopularProjects(list) {
     return returnList;
 }
 
-function createProjList(parentElement, index) {
-    var svgArr = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;width: 7%;float: right;" xml:space="preserve"><path style="fill:#B5B5B5;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111  C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587  c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"></path></svg>';
-    var listEl = document.createElement("li");
-    var nameEl = document.createElement("span");
-    var starsEl = document.createElement("span");
-    var forksEl = document.createElement("span");
-    var nameVal = document.createElement("span");
-    var starsVal = document.createElement("span");
-    var forksVal = document.createElement("span");
-    var linkEl = document.createElement("a");
+function createProjList(parentElement, index, isDesc, desc) {
+    if(!isDesc) {
+        var svgArr = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;width: 7%;float: right;" xml:space="preserve"><path style="fill:#B5B5B5;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111  C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587  c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"></path></svg>';
+        var listEl = document.createElement("li");
+        var nameEl = document.createElement("span");
+        var starsEl = document.createElement("span");
+        var forksEl = document.createElement("span");
+        var nameVal = document.createElement("span");
+        var starsVal = document.createElement("span");
+        var forksVal = document.createElement("span");
+        var linkEl = document.createElement("a");
 
-    var nameElInnerVal = document.createTextNode("Name: ");
-    var starsElInnerVal = document.createTextNode("Stars: ");
-    var forksElInnerVal = document.createTextNode("Forks: ");
-    var starsValInnerVal = document.createTextNode(projectsList[index]['watchers']);
-    var forksValInnerVal = document.createTextNode(projectsList[index]['forks']);
+        var nameElInnerVal = document.createTextNode("Name: ");
+        var starsElInnerVal = document.createTextNode("Stars: ");
+        var forksElInnerVal = document.createTextNode("Forks: ");
+        var starsValInnerVal = document.createTextNode(projectsList[index]['watchers']);
+        var forksValInnerVal = document.createTextNode(projectsList[index]['forks']);
 
-    nameEl.appendChild(nameElInnerVal);
-    starsEl.appendChild(starsElInnerVal);
-    forksEl.appendChild(forksElInnerVal);
-    nameVal.appendChild(linkEl);
-    starsVal.appendChild(starsValInnerVal);
-    forksVal.appendChild(forksValInnerVal);
-    linkEl.innerHTML = projectsList[index]['name'] + svgArr;
+        nameEl.appendChild(nameElInnerVal);
+        starsEl.appendChild(starsElInnerVal);
+        forksEl.appendChild(forksElInnerVal);
+        nameVal.appendChild(linkEl);
+        starsVal.appendChild(starsValInnerVal);
+        forksVal.appendChild(forksValInnerVal);
+        linkEl.innerHTML = projectsList[index]['name'] + svgArr;
 
-    nameEl.setAttribute("class", "projHeaders");
-    starsEl.setAttribute("class", "projHeaders");
-    forksEl.setAttribute("class", "projHeaders");
-    linkEl.setAttribute("href", "https://github.com/RAZERZ/" + projectsList[index]['name']);
+        nameEl.setAttribute("class", "projHeaders");
+        starsEl.setAttribute("class", "projHeaders");
+        forksEl.setAttribute("class", "projHeaders");
+        linkEl.setAttribute("href", "https://github.com/RAZERZ/" + projectsList[index]['name']);
 
-    listEl.appendChild(nameEl);
-    listEl.appendChild(nameVal);
-    listEl.appendChild(document.createElement("br"));
-    listEl.appendChild(starsEl);
-    listEl.appendChild(starsVal);
-    listEl.appendChild(document.createElement("br"));
-    listEl.appendChild(forksEl);
-    listEl.appendChild(forksVal);
-    listEl.appendChild(document.createElement("br"));
+        listEl.appendChild(nameEl);
+        listEl.appendChild(nameVal);
+        listEl.appendChild(document.createElement("br"));
+        listEl.appendChild(starsEl);
+        listEl.appendChild(starsVal);
+        listEl.appendChild(document.createElement("br"));
+        listEl.appendChild(forksEl);
+        listEl.appendChild(forksVal);
+        listEl.appendChild(document.createElement("br"));
 
+    }
+
+    else {
+        var svgArr = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;width: 7%;float: right;" xml:space="preserve"><path style="fill:#B5B5B5;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111  C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587  c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"></path></svg>';
+        var listEl = document.createElement("li");
+        var nameEl = document.createElement("span");
+        var descEl = document.createElement("span");
+        var nameVal = document.createElement("span");
+        var descVal = document.createElement("span");
+        var linkEl = document.createElement("a");
+
+        var nameElInnerVal = document.createTextNode("Name: ");
+        var descElInnerVal = document.createTextNode("Desc: ");
+
+        nameEl.appendChild(nameElInnerVal);
+        nameVal.appendChild(linkEl);
+        descEl.appendChild(descElInnerVal);
+        descVal.innerHTML = desc;
+        linkEl.innerHTML = projectsList[index]['name'] + svgArr;
+
+        nameEl.setAttribute("class", "projHeaders");
+        descEl.setAttribute("class", "projHeaders");
+        linkEl.setAttribute("href", "https://github.com/RAZERZ/" + projectsList[index]['name']);
+
+        listEl.appendChild(nameEl);
+        listEl.appendChild(nameVal);
+        listEl.appendChild(document.createElement("br"));
+        listEl.appendChild(descEl);
+        listEl.appendChild(descVal);
+
+    }
     parentElement.appendChild(listEl);
-
 }
