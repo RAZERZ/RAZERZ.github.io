c2be9d42dd497266d848bd03d15f21760dfb7dbd
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-01-11 23:06:48 +0100

    Added random color generator.

--- hostedProjects/KarlstadbussTicketGenerator/construct.js
+++ hostedProjects/KarlstadbussTicketGenerator/construct.js
@@ -9,7 +9,13 @@ window.onload = function() {
     else {
         createModal('<div style="background: red; padding: 10%; "><p style="font-size: 6em; margin: unset; text-align: center;">Warning!</p></div><div style=" color: white; font-size: 3em; padding: 10%;"><p>By continuing, you are hereby solely responsible for any consequences when using this online site. <p>I take no responsibility whatsoever. This shall only be used for development and or learning purposes. </p><p>Made by Rami Abou Zahra, All rights reserved.</p></p></div><button onclick="closeModal(this)" style="margin: auto; display: block; width: 90%; height: 10%; font-size: 5em; font-weight: bold; color: green; ">ACCEPT</button></div>');
     }
+
+    randomGradientOnload();
 };
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 function createModal(modalContent) {
     var modalContainer = document.createElement("div");
@@ -24,4 +30,23 @@ function createModal(modalContent) {
 
 function closeModal(modalEl) {
     modalEl.parentElement.parentElement.removeChild(modalEl.parentElement);
+}
+
+function randomGradientOnload() {
+    var gradientElement = document.getElementsByClassName("gradientContainer")[0];
+
+    switch (getRandomInt(3)) {
+        case 0:
+            gradientElement.setAttribute("style", "background: linear-gradient(90deg, rgba(34,174,251,0.9) 29%, rgba(254,136,26,0.9) 67%);");
+        break;
+        case 1:
+            gradientElement.setAttribute("style", "background: linear-gradient(90deg, rgba(0, 132, 237, 0.9) 20%, rgba(59, 162, 97, 0.9) 50%, rgba(190, 217, 0, 0.90) 65%);");
+        break;
+        case 2:
+            gradientElement.setAttribute("style", "background: linear-gradient(90deg, rgba(32,174,250,0.9) 29%, rgba(253,45,157,0.9) 67%);");
+        break;
+        case 3:
+            gradientElement.setAttribute("style", "background: linear-gradient(90deg, rgba(255,136,27,0.9) 29%, rgba(129,16,194,0.9) 67%);");
+        break;
+    }
 }
