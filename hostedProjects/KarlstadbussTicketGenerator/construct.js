be7b4ca38424af7d585d3eb85ac905459e004576
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-01-11 23:44:20 +0100

    Removed scawy popup uwu

--- hostedProjects/KarlstadbussTicketGenerator/construct.js
+++ hostedProjects/KarlstadbussTicketGenerator/construct.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
     if(isInWebAppiOS === false && isInWebAppChrome === false) {
         //document.body.innerHTML = "";
-        createModal('<div style="background: red; padding: 10%; "><p style="font-size: 6em; margin: unset; text-align: center;">Instructions</p></div><div style=" color: white; font-size: 3em; padding: 10%;"><p>For best experience (and because I could not figure out how the fullscreen function works in IOS), <span style="color: lime; font-weight: bolder;">save the site to your home screen and launch it from there.</span><p>You will continue to see this message until you do so.</p><p>Made by Rami Abou Zahra, All rights reserved.</p></p></div></div>');
+        createModal('<div style="background: #282828; padding: 10%; "><p style="font-size: 6em; font-weight: bolder; margin: unset; text-align: center;">Instruktioner</p></div><div style="font-size: 3em; padding: 10%;"><h1 style="margin: unset; font-size: 1.5em;">iOS</h1><p>1. Öppna länken i Safari</p><p>2. Klicka på "dela" knappen.</p><p>3. Scrolla ner och klicka på "lägg till på hemskärmen"</p><h1 style="margin: unset; font-size: 1.5em;">Android</h1><p>1. Öppna länken i Chrome.</p><p>2. Klicka på de 3 plupparna i det högra hörnet.</p><p>3. Klick på "lägg till på startskärmen"</p></div></div>');
     }
     else {
         createModal('<div style="background: red; padding: 10%; "><p style="font-size: 6em; margin: unset; text-align: center;">Warning!</p></div><div style=" color: white; font-size: 3em; padding: 10%;"><p>By continuing, you are hereby solely responsible for any consequences when using this online site. <p>I take no responsibility whatsoever. This shall only be used for development and or learning purposes. </p><p>Made by Rami Abou Zahra, All rights reserved.</p></p></div><button onclick="closeModal(this)" style="margin: auto; display: block; width: 90%; height: 10%; font-size: 5em; font-weight: bold; color: green; ">ACCEPT</button></div>');
@@ -21,7 +21,7 @@ function createModal(modalContent) {
     var modalContainer = document.createElement("div");
 
     modalContainer.setAttribute("class", "modalContainer");
-    modalContainer.setAttribute("style", "position: absolute; top: 0; left: 0; background: black; z-index: 999; width: " + window.innerWidth + "px; height: " + window.innerHeight + "px;");
+    modalContainer.setAttribute("style", "position: absolute; top: 0; left: 0; background: #212121; z-index: 999; width: " + window.innerWidth + "px; height: " + window.innerHeight + "px; color: white; font-family: Comfortaa;");
 
     modalContainer.innerHTML = modalContent;
 
