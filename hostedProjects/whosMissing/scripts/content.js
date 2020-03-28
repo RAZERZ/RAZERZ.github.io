b86507d9a94a3cbf8d73d182bd28f4392a310c5a
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-03-28 23:32:19 +0100

    Added ability to eval ajax fetched scripts

--- hostedProjects/whosMissing/scripts/content.js
+++ hostedProjects/whosMissing/scripts/content.js
@@ -2,12 +2,53 @@ var headerContainer = document.getElementById("header");
 var contentContainer = document.getElementById("content");
 var headerPointer = headerContainer.getElementsByTagName("h1")[0];
 
-function getSite(module) {
-    var ajaxReq = new XMLHttpRequest();
-    ajaxReq.open("GET", "modules/" + module + ".html", false);
-    ajaxReq.send();
-    return ajaxReq.responseText;
+function setSiteContent(file) {
+
+    contentContainer.innerHTML = '<div class="dotLoading"><span class="dotLoading a">.</span><span class="dotLoading b">.</span><span class="dotLoading c">.</span></div>';
+
+    if(window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    }
+    else {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlHttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            contentContainer.innerHTML = this.responseText;
+            var scripts = contentContainer.getElementsByTagName("script");
+            for(var i=0; scripts.length > i; i++ ) {
+                eval(scripts[i].innerText);
+            }
+        }
+    };
+
+    xmlHttp.open("GET", "modules/" + file + ".html", true);
+    xmlHttp.send();
+
 }
 
-window.addEventListener("load",function(){contentContainer.innerHTML=getSite("init")});
-headerPointer.addEventListener("click",function(){contentContainer.innerHTML=getSite("init")});
+function loadingAnimation(pEl) {
+    pEl.innerText = "";
+    var container = document.createElement("div");
+    var alpha = document.createElement("span");
+    var bravo = document.createElement("span");
+    var charlie = document.createElement("span");
+
+    container.appendChild(alpha);
+    container.appendChild(bravo);
+    container.appendChild(charlie);
+    alpha.appendChild(document.createTextNode("."));
+    bravo.appendChild(document.createTextNode("."));
+    charlie.appendChild(document.createTextNode("."));
+    pEl.appendChild(container);
+
+    container.setAttribute("class", "dotLoading");
+    alpha.setAttribute("class", "dotLoading a");
+    bravo.setAttribute("class", "dotLoading b");
+    charlie.setAttribute("class", "dotLoading c");
+}
+
+window.addEventListener("load",function(){contentContainer.innerHTML=setSiteContent("init")});
+headerPointer.addEventListener("click",function(){contentContainer.innerHTML=setSiteContent("init")});
+
